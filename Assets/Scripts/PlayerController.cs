441c87cfd895a067c42c3739c3c069782ade2cc8
Astroneer112 <sam.foster.johnson@hotmail.com>
2026-03-16 14:27:56 -0700

    sprinting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool sprinting = false;
+    float sprintMult = 3.0f;
+    float stamina = 100.0f;
+    float stamDrain = 30.0f;
+    float stamRegen = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,7 +30,26 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.D)){
             movement += new Vector3(1f,0,0);
         }
+        movement = Vector3.Normalize(movement);
 
-        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.Normalize(movement)/0.5f);
+        if(Input.GetKey(KeyCode.LeftShift)){
+            sprinting = true;
+        } else{
+            sprinting = false;
+            if(stamina + stamRegen * Time.deltaTime < 100){
+                stamina += stamRegen * Time.deltaTime;
+            } else{
+                stamina = 100.0f;
+            }
+            
+        }
+
+        if(sprinting && stamina - stamDrain * Time.deltaTime > 0){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(movement*sprintMult);
+            stamina = stamina - stamDrain * Time.deltaTime;
+        } else{
+            gameObject.GetComponent<Rigidbody2D>().AddForce(movement);
+        }
+        Debug.Log("Current stamina: " + stamina);
     }
 }
