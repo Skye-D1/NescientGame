a758947eeaf4eacba10c642d1826dc865df0ba52
Astroneer112 <sam.foster.johnson@hotmail.com>
2026-03-19 14:20:05 -0700

    projectile trail stuff

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ public class Projectile : MonoBehaviour
 {
     public float timer = 0.25f;
     bool stopped = false;
+    Vector3 randomDir = new Vector3();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,10 +18,15 @@ public class Projectile : MonoBehaviour
         if(!stopped && timer <= 0){
             gameObject.GetComponent<Rigidbody2D>().linearVelocity = new Vector3();
             stopped = true;
-            timer = 0.25f;
+            timer = 0.75f;
         }
         else if (stopped && timer <= 0){
             GameObject.Destroy(gameObject);
+        }
+        if(stopped){
+            transform.position -= randomDir;
+            randomDir = new Vector3(Random.Range(-0.15f, 0.15f), Random.Range(-0.15f, 0.15f), 0);
+            transform.position += randomDir;
         }
     }
 }
