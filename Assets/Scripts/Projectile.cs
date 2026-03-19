3f1ef1d61fcf5eea420dc7dfd0c89b9fd35ce745
Astroneer112 <sam.foster.johnson@hotmail.com>
2026-03-19 13:51:39 -0700

    projectile trails and randomization of angle/speed

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    float timer = 0.25f;
+    public float timer = 0.25f;
+    bool stopped = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +14,12 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
-        if (timer <= 0){
+        if(!stopped && timer <= 0){
+            gameObject.GetComponent<Rigidbody2D>().linearVelocity = new Vector3();
+            stopped = true;
+            timer = 0.25f;
+        }
+        else if (stopped && timer <= 0){
             GameObject.Destroy(gameObject);
         }
     }
