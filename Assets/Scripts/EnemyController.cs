7612f5c2a25e5dc2bdae76745fa83b6e1af19cdd
Skye-D1 <skye.drury@digipen.edu>
2026-03-20 13:16:13 -0700

    enemies can plant

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,12 +11,14 @@ public class EnemyController : MonoBehaviour
     Vector3 movement = new Vector3(); // current movement direction
     public bool targetPriority; // whether the target is high priority (player noise)
     bool didTargetUpdate; // whether the enemy has updated target this frame
+    float overshootTimer; // time left moving past target
     public float echoRadius; // radius to echo recieved noise to others
     LayerMask enemyMask; // layermask for finding other enemies
     Collider2D[] enemiesFound; // enemies found wjen circle
     public bool isStatic; // if enemy does not move for tutorial
     public bool isLeader; // if enemy will create wander targets
     LineRenderer lineRenderer;
+    public GameObject staticPlant; // object to replace enemy with
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,8 +36,22 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         didTargetUpdate = false; // allow one target update per frame
+        
+        // start moving past target for a time when reached
+        if (Vector2.Distance(target, new Vector2(transform.position.x, transform.position.y)) < 1f && overshootTimer <= 0 && movement.magnitude > 0) {
+            //Debug.Log("Enemy: overshoot " + movement);
+            overshootTimer = 3f;
+        }
+
+        // keep moving past target for a bit
+        if (overshootTimer > 0) {
+            overshootTimer -= Time.deltaTime;
+            lineRenderer.enabled = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(movement * Time.deltaTime);
+            target = new Vector2(transform.position.x, transform.position.y);
+            targetPriority = false;
         // move towards target
-        if (!isStatic) {
+        } else if (!isStatic) {
             movement = Vector3.Normalize(new Vector3(target.x, target.y, 0) - transform.position) * moveSpeed;
             gameObject.GetComponent<Rigidbody2D>().AddForce(movement * Time.deltaTime);
             lineRenderer.enabled = true;
@@ -44,6 +60,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    // hear a noise and update target if necessary
     public void recieveNoise(Vector2 newTarget, bool newTargetPriority)
     {
         // switch target if new is priority or current is null
@@ -57,6 +74,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    // communicate some recieved noises to other enemies nearby
     void echoNoise(Vector2 newTarget, bool newTargetPriority) {
         // overlap circle to check for enemy tag
         enemiesFound = Physics2D.OverlapCircleAll(transform.position, echoRadius, enemyMask); // the aforementioned circle
@@ -64,5 +82,13 @@ public class EnemyController : MonoBehaviour
         for(int i = 0; i < enemiesFound.Length; i++) {
             enemiesFound[i].gameObject.GetComponent<EnemyController>().recieveNoise(newTarget, newTargetPriority);
         }
+    }
+
+    // recieve signal from colliding water to become static plant
+    void plantify () {
+        // spawn static plant
+        Instantiate(staticPlant, transform.position, transform.rotation);
+        // delete self
+        GameObject.Destroy(gameObject);
     }
 }
