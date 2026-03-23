f4ce1eff9eda881fa5471916df3fa5caa712408f
Astroneer112 <sam.foster.johnson@hotmail.com>
2026-03-23 14:26:52 -0700

    LAYERING!!!

--- Assets/Scripts/layerSetStatic.cs
+++ Assets/Scripts/layerSetStatic.cs
@@ -5,12 +5,6 @@ public class layerSetStatic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = -(int)Mathf.Floor((transform.position.y + gameObject.GetComponent<Collider2D>().offset.y)*10);
     }
 }
