68d07a158264ff3ed0dffb4a4d454349e64555ca
ACGdigi <aidan.gillette@digipen.edu>
2026-03-27 14:01:17 -0700

    pause menu 2.0?

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,4 +1,6 @@
+//using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 //Name: Aidan Gillette
 //File: PauseMenu.cs
@@ -6,15 +8,53 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject PauseMenuPanel;
+    public bool isPaused = false;
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PauseMenuPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
+
+    public void PauseGame()
+    {
+        PauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+
+    public void ResumeGame()
+    {
+        PauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
