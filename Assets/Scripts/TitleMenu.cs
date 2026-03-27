6d5e36cacf607dc021d361e86031d00cd837720f
Skye-D1 <skye.drury@digipen.edu>
2026-03-27 14:54:25 -0700

    title meu

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Name: Skye Drury
+//File: TitleMenu.cs
+//Purpose: menu for title screen scene
+
+public class TitleMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Button pbutton = GameObject.Find("Play button").GetComponent<Button>();
+        pbutton.onClick.AddListener(playFunction);
+        
+        Button creditsbutton = GameObject.Find("Credits").GetComponent<Button>();
+        creditsbutton.onClick.AddListener(creditsFunction);
+        
+        Button quitbutton = GameObject.Find("Quit").GetComponent<Button>();
+        quitbutton.onClick.AddListener(quitFunction);
+        
+        Debug.Log("setup complete");
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        }
+    }
+
+    // load main scene
+    void playFunction() {
+        Debug.Log("meow");
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    // 
+    void creditsFunction() {
+        Debug.Log("credits time");
+    }
+
+    // quit game
+    void quitFunction() {
+        Application.Quit();
+    }
+
+    
+}
