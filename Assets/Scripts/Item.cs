4044790d4b6e76b0caac93e4d01fa94175bc0e55
Skye-D1 <skye.drury@digipen.edu>
2026-04-02 13:35:02 -0700

    glow fix

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,16 +7,30 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public int itemID = 0; // what item is it?
-    public float power = 0; // heal amount, water level, durability, etc...
-    float pulseSpeed = 1f; // speed for glow pulse
-    float glowOpacity;
+    public float power = 0f; // heal amount, water level, durability, etc...
+    float opacityRate = 1f; // speed for glow pulse
+    float glowOpacity = 0f; // current glow opacity
     SpriteRenderer glowSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        glowSprite = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
         if(transform.parent != null){
             gameObject.GetComponent<SpriteRenderer>().sortingOrder = 20000;
+            opacityRate = 0;
         }
+    }
+
+    // yea
+    void Update()
+    {
+        glowOpacity += Time.deltaTime * opacityRate;
+        // opacity limit and flip direction on limit
+        if (glowOpacity > 1 || glowOpacity < 0) {
+            glowOpacity = glowOpacity > 0.5 ? 1 : 0;
+            opacityRate *= -1;
+        }
+        glowSprite.color = new Color(1f,1f,1f,(glowOpacity));
     }
 }
