ddc4bf89d687a178b076ec64368dd3ac6117ab22
Astroneer112 <sam.foster.johnson@hotmail.com>
2026-04-03 13:30:10 -0700

    made thirst tweak

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -10,12 +10,12 @@ public class CameraEffects : MonoBehaviour
 {
     public Volume volume;
     public Vignette vignette;
-    ColorAdjustments saturation;
+    ColorAdjustments desaturation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         volume.profile.TryGet(out vignette);
-        volume.profile.TryGet(out saturation);
+        volume.profile.TryGet(out desaturation);
     }
 
     // Update is called once per frame
@@ -30,13 +30,19 @@ public class CameraEffects : MonoBehaviour
         
         //stamina vignette
         if(!GameObject.Find("Player").GetComponent<PlayerController>().isDying()){
-            //Debug.Log("camera check: " + GameObject.Find("Player").GetComponent<PlayerController>().isDying());
             float Stamina = GameObject.Find("Player").GetComponent<PlayerController>().Stamina;
             vignette.intensity.value = (100f-Stamina)/175f;
         }
 
         //thirst desaturation
         float Thirst = GameObject.Find("Player").GetComponent<PlayerController>().Thirst;
-        saturation.saturation.value = -(100f-Thirst);
+        //desaturation.saturation.value = -(100f-Thirst);
+        if(Thirst >= 50f){
+            desaturation.saturation.value = 0;
+        } else if(Thirst < 20f){
+            desaturation.saturation.value = -100f;
+        } else{
+            desaturation.saturation.value = -(100f - ((Thirst-20f) * (10f/3f)));
+        }
     }
 }
