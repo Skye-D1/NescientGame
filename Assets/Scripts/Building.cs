738e0f2dcd394dad01f6959c42a3affeaac6cee1
Astroneer112 <88603578+Astroneer112@users.noreply.github.com>
2026-04-10 13:26:08 -0700

    fixed house back wall layering, added tree leaves transparency upon player walk-behind

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -10,8 +10,9 @@ public class Building : MonoBehaviour
     public float outRadius;
     public float inRadius;
     SpriteRenderer roofSprite;
-    public float offset;
+    public float sortingOrderOffset;
     int frame = 0;
+    public Vector2 transparencyRadiusOffset;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,9 +29,9 @@ public class Building : MonoBehaviour
         }
         if(frame == 3){
             frame += 1;
-            roofSprite.sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder + (int)Mathf.Floor(offset * 10f);
+            roofSprite.sortingOrder = gameObject.GetComponent<SpriteRenderer>().sortingOrder + (int)Mathf.Floor(sortingOrderOffset * 10f);
         }
-        float distance = Vector3.Distance(player.transform.position, transform.position);
+        float distance = Vector2.Distance(new Vector2(player.transform.position.x, player.transform.position.y), new Vector2(transform.position.x, transform.position.y) + transparencyRadiusOffset);
         if(distance-inRadius < outRadius-inRadius){
             roofSprite.color = new Color(1f, 1f, 1f, (distance-inRadius) / (outRadius-inRadius));
         }
