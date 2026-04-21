37ad4e9105954913e79a77017695360f73ee5b59
Skye-D1 <skye.drury@digipen.edu>
2026-04-21 14:34:08 -0700

    UI sprites added, dynamic layering for projectile trails, other sprite updates

---
+++ Assets/Scripts/TrailLayerSetDynamic.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System;
+
+//Name: Sam Johnson, Skye Drury
+//File: TrailLayerSetDynamic.cs
+//Purpose: Manages the sortingLayer of the trailrenderer on objects that have a dynamic position
+
+public class TrailLayerSetDynamic : MonoBehaviour
+{
+    TrailRenderer Renderer;
+    Collider2D Collider;
+    public float offset;
+    bool hasCollider = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Renderer = gameObject.GetComponent<TrailRenderer>();
+        try{
+            Collider = gameObject.GetComponent<Collider2D>();
+        } catch(Exception E){
+            hasCollider = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hasCollider){
+            Renderer.sortingOrder = -(int)Mathf.Floor((transform.position.y + Collider.offset.y + offset)*10);
+        }
+        else{
+            Renderer.sortingOrder = -(int)Mathf.Floor((transform.position.y + offset)*10);
+        }
+    }
+}
